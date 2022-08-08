9f5f80214b1799def73bbab6a3fa9f958e313f26
AndreyChernykov <totktogovoryt@yandex.ru>
2022-08-08 19:24:55 +0300

    AmmoBox

--- Assets/Scripts/ForArena/AmmoBoxArena.cs
+++ Assets/Scripts/ForArena/AmmoBoxArena.cs
@@ -7,27 +7,39 @@ public class AmmoBoxArena : MonoBehaviour
     [SerializeField] GameObject[] ammoType;    
     [SerializeField] int minCartriges;
     [SerializeField] int maxCartriges;
+    [SerializeField] AudioClip crashedAudio;
+    AudioSource audioSource;
     GameObject ammo;
     private float posY = 0.2f;
 
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         GameObject ammoPref = ammoType[Random.Range(0, ammoType.Length)];
         ammo = Instantiate(ammoPref);
-        //int amountBullet = ammo.GetComponent<AmmoBox>().AmountBullet;
         ammo.GetComponent<AmmoBox>().AmountBullet = Random.Range(minCartriges, minCartriges + 1);
         ammo.transform.SetParent(gameObject.transform);
         ammo.transform.position = new Vector3(transform.position.x, transform.position.y + posY, transform.position.z);
+        
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "bullet")
         {
+
             ammo.transform.parent = null;
-            OnDestroy();
-            
+            ammo.AddComponent<Rigidbody>();
+            audioSource.PlayOneShot(crashedAudio);
+
+            Invoke("OnDestroy", 0.3f);
+
         }
+        else
+        {
+            audioSource.Play();
+        }
+
     }
 
     private void OnDestroy()
